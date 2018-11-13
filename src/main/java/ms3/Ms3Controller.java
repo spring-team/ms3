b26e25a027fa39c7bc54a458e3786535297f8b49
Rod Johnson <johnsonroda@gmail.com>
2018-11-13 13:35:54 -0800

    Initial commit from Atomist

---
+++ src/main/java/ms3/Ms3Controller.java
@@ -0,0 +1,20 @@
+package ms3;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class Ms3Controller {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello, Delta Blues! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
